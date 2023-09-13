f36d3c3092c398b5f8ac28e453b466328604675e
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-13 15:08:07 +0200

    Refactor signinform.js

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 import Form from "react-bootstrap/Form";
@@ -17,31 +17,30 @@ import appStyles from "../../App.module.css";
 
 function SignInForm() {
   const [signInData, setSignInData] = useState({
-    username: '',
-    password: '',
+    username: "",
+    password: "",
   });
-
-  const history = useHistory();  
   const { username, password } = signInData;
-  
+
   const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/login/", signInData);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
 
   const handleChange = (event) => {
     setSignInData({
       ...signInData,
       [event.target.name]: event.target.value,
     });
-  }
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      await axios.post('/dj-rest-auth/login/', signInData)
-      history.push('/');
-    } catch(err){
-      setErrors(err.response?.data)
-    }
-  }
+  };
 
   return (
     <Row className={styles.Row}>
@@ -52,49 +51,48 @@ function SignInForm() {
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
-              className={styles.Input} 
-              type="text" 
-              placeholder="Username" 
-              name="username"
-              value={username}
-              onChange={handleChange}
+                type="text"
+                placeholder="Username"
+                name="username"
+                className={styles.Input}
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
-
             {errors.username?.map((message, idx) => (
-            <Alert variant="warning" key={idx}>{message}</Alert>
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
             ))}
 
             <Form.Group controlId="password">
               <Form.Label className="d-none">Password</Form.Label>
-              <Form.Control 
-              className={styles.Input} 
-              type="password" 
-              placeholder="Password" 
-              name="password" 
-              value={password}
-              onChange={handleChange}
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                className={styles.Input}
+                value={password}
+                onChange={handleChange}
               />
             </Form.Group>
-
             {errors.password?.map((message, idx) => (
-            <Alert variant="warning" key={idx}>{message}</Alert>
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
             ))}
-
-            <Button 
-            className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} 
-            type="submit"
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
             >
-              Sign In
+              Sign in
             </Button>
-            {/* Non Field Errors are for when passwords don't match */}
             {errors.non_field_errors?.map((message, idx) => (
-              <Alert variant="warning" key={idx} className="mt-3">
+              <Alert key={idx} variant="warning" className="mt-3">
                 {message}
               </Alert>
             ))}
           </Form>
-
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signup">
