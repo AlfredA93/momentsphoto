a45ad195ab20da4fb5ba6c82f7b6383d093aa00e
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-14 15:03:07 +0200

    refactor postspage.js

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -5,50 +5,51 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
-import appStyles from "../../App.module.css";
-import styles from "../../styles/PostsPage.module.css";
-import { useLocation } from "react-router-dom/cjs/react-router-dom";
-import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
-
-import NoResults from '../../assets/no-results.png';
 import Asset from "../../components/Asset";
 
-function PostsPage({message, filter = ""}) {
+import appStyles from "../../App.module.css";
+import styles from "../../styles/PostsPage.module.css";
+import { useLocation } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
-  const [posts, setPosts] = useState({results: []});
+import NoResults from "../../assets/no-results.png";
+
+function PostsPage({ message, filter = "" }) {
+  const [posts, setPosts] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const {data} = await axiosReq.get(`/posts?${filter}`);
+        const { data } = await axiosReq.get(`/posts/?${filter}`);
         setPosts(data);
         setHasLoaded(true);
-      } catch(err){
+      } catch (err) {
         console.log(err);
       }
     };
+
     setHasLoaded(false);
     fetchPosts();
   }, [filter, pathname]);
-  
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
         {hasLoaded ? (
           <>
-          {posts.results.length ? (
-            posts.results.map(post => (
-              <Post key={post.id} {...post} setPosts={setPosts} />
-            ))
-          ) : (
-            <Container className={appStyles.Content}>
-              <Asset src={NoResults} message={message} />
-            </Container>
-          )}
+            {posts.results.length ? (
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
           </>
         ) : (
           <Container className={appStyles.Content}>
