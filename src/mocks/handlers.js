e4e942a02c03e9c292c1550805ae07ed656d0d40
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-16 13:08:59 +0200

    Set up testing environment

---
+++ src/mocks/handlers.js
@@ -0,0 +1,20 @@
+import { rest } from "msw";
+
+const baseURL = 'https://drf-api-alf-f7affde58488.herokuapp.com/';
+
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req,res,ctx) => {
+    return res(ctx.json({
+      "pk": 9,
+      "username": "Harry",
+      "email": "",
+      "first_name": "",
+      "last_name": "",
+      "profile_id": 9,
+      "profile_image": "https://res.cloudinary.com/dl80utlk8/image/upload/v1/media/../default_profile_oztffo"
+      }));
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (res,res,ctx) => {
+    return res(ctx.status(200));
+  }),
+];
