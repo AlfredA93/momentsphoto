10ae7f0ec4062a22441a6cbba1afbc2c53b9e091
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-16 15:33:09 +0200

    Add localStorage functions

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -16,11 +16,11 @@ import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import { useRedirect } from "../../hooks/useRedirect";
+import { setTokenTimestamp } from "../../utils/utils";
 
 function SignInForm() {
-
   const setCurrentUser = useSetCurrentUser();
-  useRedirect('loggedIn');
+  useRedirect("loggedIn");
 
   const [signInData, setSignInData] = useState({
     username: "",
@@ -33,9 +33,11 @@ function SignInForm() {
   const history = useHistory();
   const handleSubmit = async (event) => {
     event.preventDefault();
+
     try {
-      const {data} = await axios.post("/dj-rest-auth/login/", signInData);
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
+      setTokenTimestamp(data);
       history.goBack();
     } catch (err) {
       setErrors(err.response?.data);
