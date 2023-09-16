fd28b73cbb8869af691d0b29a0fba954f45fea8d
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-09-16 10:29:57 +0200

    Add unfollow button functionality

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,11 +6,11 @@ export const fetchMoreData = async (resource, setResource) => {
     setResource((prevResource) => ({
       ...prevResource,
       next: data.next,
-      results: data.results.reduce((acc, cur) => {  // Look through results from API with reduce()
-        return acc.some((accResult) => accResult.id === cur.id)  //Use some() to compare current and new post id's
-          ? acc  // if some returns true, then it does not add post to the list of results
-          : [...acc, cur];  // if some returns false, it adds post to the list of results
-      }, prevResource.results),  // append to current results in State
+      results: data.results.reduce((acc, cur) => {
+        return acc.some((accResult) => accResult.id === cur.id)
+          ? acc
+          : [...acc, cur];
+      }, prevResource.results),
     }));
   } catch (err) {}
 };
@@ -28,6 +28,24 @@ export const followHelper = (profile, clickedProfile, following_id) => {
     ? // This is the profile of the logged in user
       // update its following count
       { ...profile, following_count: profile.following_count + 1 }
+    : // this is not the profile the user clicked on or the profile
+      // the user owns, so just return it unchanged
+      profile;
+};
+
+export const unfollowHelper = (profile, clickedProfile) => {
+  return profile.id === clickedProfile.id
+    ? // This is the profile I clicked on,
+      // update its followers count and set its following id
+      {
+        ...profile,
+        followers_count: profile.followers_count - 1,
+        following_id: null,
+      }
+    : profile.is_owner
+    ? // This is the profile of the logged in user
+      // update its following count
+      { ...profile, following_count: profile.following_count - 1 }
     : // this is not the profile the user clicked on or the profile
       // the user owns, so just return it unchanged
       profile;
